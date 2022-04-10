dad982785009f0f3493c259710c4b1d097c4a061
Heimdall333 <84260339+Heimdall333@users.noreply.github.com>
2022-04-10 12:49:43 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,5 +1,10 @@
-#Q1
+#Q1 Write a Python program which accepts the radius of a circle from the user and computes area.
 
 r = float(input("Input the radius of the circle = "))
 a = 3.14159 * r * r
 print("The area of the circle with radius is = ", a)
+
+#Q2 Write a Python program to accept a filename from the user and print the extension of that.
+filename = input("Input the Filename: ")
+file_extn = filename.split(".")
+print ("The extension of the file is : " + repr(file_extn[-1]))
