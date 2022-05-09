434a4f28595ab5c7f4a63c752a76982d2eb84abd
Atharva Patil <84260339+Heimdall333@users.noreply.github.com>
2022-05-09 10:28:43 +0530

    Update Loop.py

--- Loop.py
+++ Loop.py
@@ -1,3 +1,4 @@
+#1
 def Fibonacci(n): 
     if n<0: 
         print("Incorrect input") 
@@ -11,3 +12,19 @@ def Fibonacci(n):
         return Fibonacci(n-1) + Fibonacci(n-2) 
 
 print(Fibonacci(9)) 
+
+#2 a)
+list1 = [12, -7, 5, 64, -14]
+num = 0     
+while(num < len(list1)):
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+    num += 1
+    
+#2 b)
+list2 = [12, 14, -95, 3]
+num = 0     
+while(num < len(list2)):
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+    num += 1
